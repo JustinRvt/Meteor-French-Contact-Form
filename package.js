e26edf26e1bd35a9a1e4d0193452440cfaaba37e
JustinRvt <justin@ravat.fr>
2016-06-20 17:33:31 +0200

    fix package.js issue with Coffeescript & Jade

--- package.js
+++ package.js
@@ -2,18 +2,22 @@ Package.describe({
     name: 'justinrvt:contact-form',
     summary: 'French contact form for METEOR',
     version: '1.3.3',
-    git: 'https://github.com/JustinRvt/Meteor-French-Contact-Form'
+    git: 'https://github.com/JustinRvt/Meteor-French-Contact-Form',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('1.3.3');
     api.use('email', 'server');
     api.use([
-        "templating",
-        "mquandalle:jade@0.4.9"
-    ], "client");
+        'templating',
+        'mquandalle:jade@0.4.9'
+    ]);
+    api.use([
+      'templating',
+      'coffeescript'
+    ]);    
     api.use('aldeed:autoform@5.3.0');
-    api.use('coffeescript');
     api.addFiles('contact-form.coffee');
     api.addFiles([
             'client/views/contact-form.jade',
